31d9988acb880a982004b935ab80a69aa62f5cc6
AntonioMora <126864866+Antoniomorales17@users.noreply.github.com>
2023-05-25 19:30:41 +0000

    commit

--- src/js/index.js
+++ src/js/index.js
@@ -5,8 +5,23 @@ import ReactDOM from "react-dom";
 // include your styles into the webpack bundle
 import "../styles/index.css";
 
+
+function SimpleCounter (props) {
+    return(<div className="bigCounter">
+       
+        <div className="six"></div>
+        <div className="calender">
+        <i class="bi bi-clock-fill"></i></div>
+        <div className="four"></div>
+        <div className="three"></div>
+        <div className="two"></div>
+        <div className="one"></div>
+
+    </div>)
+}
+
 //import your own components
-import Home from "./component/home.jsx";
+import {home} from "./component/home.jsx";
 
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+ReactDOM.render(<SimpleCounter />, document.querySelector("#app"));
